5bd792407ba73a9adb9f5fa6a32105ebee4dd731
pinodo <palifarec@gmail.com>
2026-05-29 16:46:55 +0900

    added SQL ddl, dml examples

---
+++ ex02_dml.sql
@@ -0,0 +1,17 @@
+USE member_db;
+
+-- INSERT
+INSERT INTO members (mem_id, mem_name, mem_number, addr, phone1, phone2, height, debut_date)
+VALUES ('12345678', '홍길동', 1, 'KR', '010', '44445555', 100, NOW()); -- NOW(): INSERT가 실행된 시간, SYSDATE(): SYSDATE가 실행된 시간
+
+-- UPDATE
+UPDATE members
+SET mem_name = '김철수'
+WHERE mem_id = '12345678'; -- 조건은 PK를 사용할 것
+
+INSERT INTO visit_history (mem_id, visited_at)
+VALUES ('12345678', NOW());
+
+-- DELETE
+DELETE FROM members
+WHERE mem_id = '12345678';
